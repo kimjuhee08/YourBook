9b970eebf08a9ca472845f113b1c77e7dd2b5581
Kumushai9919 <kumushnaz@gmail.com>
2022-11-08 18:40:08 +0900

    sdf

---
+++ app/src/main/java/com/example/yourbook/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.yourbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import com.example.yourbook.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    //view binding
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //handle loginBtn click, start login screen
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        //handle skipBtn click, start continue without login screen
+        binding.skipBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+    }
+}
